9d40932549df9d9e8e448eb83f76844d4ded6aeb
須藤百波 <sudomonami@gmail.com>
2020-04-22 12:40:48 +0900

    コロナウイルスについて、ファビコン、スマホ版の詳細部調整  追加

--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@ $(function () {
     $(document) //スマホ無効
       .on("scroll", scrollAnimation)
       .on("scroll", headerScrollBar);
-  } else {
-    $(document)
-      .on("click", ".l-ham", hamburgerMenu);
-  }    
+  }
+
+  $(document)
+    .on("click", ".l-ham", hamburgerMenu);  
 
   // ハンバーガーメニュー
   function hamburgerMenu() {
@@ -42,5 +42,15 @@ $(function () {
       $(".l-header").removeClass("l-header__scrollBottom");
     }
   }
+
+  // kozuka-gothic-pro 追加 
+  (function (d) {
+    var config = {
+    kitId: 'znm3odn',
+  scriptTimeout: 3000,
+  async: true
+  },
+  h = d.documentElement, t = setTimeout(function () {h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"; }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a; h.className += " wf-loading"; tk.src = 'https://use.typekit.net/' + config.kitId + '.js'; tk.async = true; tk.onload = tk.onreadystatechange = function () {a = this.readyState; if (f || a && a != "complete" && a != "loaded") return; f = true; clearTimeout(t); try {Typekit.load(config)} catch (e) {} }; s.parentNode.insertBefore(tk, s)
+  })(document);
 
 });
